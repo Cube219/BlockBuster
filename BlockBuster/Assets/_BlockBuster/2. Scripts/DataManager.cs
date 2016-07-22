52394dd610a10b3c2bd42f26861a93d73666ee70
Cube219 <max804@naver.com>
2016-07-22 15:30:18 +0900

    User / DataManager 추가

---
+++ BlockBuster/Assets/_BlockBuster/2. Scripts/DataManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataManager {
+
+	public static string accountType;
+	public static string email;
+	public static string password;
+
+	// 계정 정보를 불러옴
+	public static void LoadAccountData()
+	{
+		// 저장된 계정 정보를 불러온다
+		accountType = PlayerPrefs.GetString("accountType", "NoData");
+
+		if(accountType.Equals("NoData")) { // 계정 정보가 없다
+
+		} else {
+			switch(accountType) {
+				case "Email":
+					email = PlayerPrefs.GetString("email");
+					password = PlayerPrefs.GetString("password");
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
+
+	// 계정 정보를 저장
+	public static void SaveAccountData()
+	{
+		PlayerPrefs.SetString("accountType", accountType);
+
+		switch(accountType) {
+			case "Email":
+				PlayerPrefs.SetString("email", email);
+				PlayerPrefs.SetString("password", password);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+}
---
+++ BlockBuster/Assets/_BlockBuster/2. Scripts/UserManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserManager {
+
+	public static string uid;
+	public static string sid;
+}
