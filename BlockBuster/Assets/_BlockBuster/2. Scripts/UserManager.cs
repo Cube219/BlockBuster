c4252319e035ac6140913de8e061bda272502e18
Cube219 <max804@naver.com>
2016-07-22 17:17:19 +0900

    UseManager에 값 추가(name, userType, accountType) API 일부 수정

--- BlockBuster/Assets/_BlockBuster/2. Scripts/UserManager.cs
+++ BlockBuster/Assets/_BlockBuster/2. Scripts/UserManager.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class UserManager {
 
-	public static string uid;
-	public static string sid;
-
 	public enum State { NotLogin, Login };
 	public static State userState = State.NotLogin;
+
+	public static string uid;
+	public static string sid;
+	public static string name;
+	public static string usertype;
+	public static string accountType;
 }
